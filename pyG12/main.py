a7dc082116efd652ea77c6647430af83f55d7df7
Davi10556 <showdavimaster@gmail.com>
2025-10-31 18:36:26 -0300

    Add files via upload

---
+++ pyG12/main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+def main():
+    janela = tk.Tk()
+    janela.title("pyG12")
+    tk.Label(janela, text="OI", font=("Arial", 30)).pack(padx=50, pady=50)
+    janela.mainloop()
